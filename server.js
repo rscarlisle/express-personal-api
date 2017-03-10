1ddffd229b53946ff75d6cc1846843b159f0eee3
namelessprofit <posadaarchy@gmail.com>
2017-03-10 09:52:21 -0800

    added heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // require express and other modules
 var express = require('express'),
     app = express();
-
+app.use(express.static('public'));
 // parse incoming urlencoded form data
 // and populate the req.body object
 var bodyParser = require('body-parser');
@@ -18,16 +18,22 @@ app.use(function(req, res, next) {
 /************
  * DATABASE *
  ************/
-
+var headGear = [
+{_id:0, name: "fedora", color: "dark_brown", ribbon:true, ribbon_color: "brown"},
+{_id:1, name: "fedora", color: "black", ribbon:true, ribbon_color: "black"},
+{_id:2, name: "fedora", color: "grey", ribbon:true, ribbon_color: "black"},
+{_id:3, name: "fedora", color: "burgundy", ribbon:true, ribbon_color: "grey"},
+];
 // var db = require('./models');
 
 /**********
  * ROUTES *
  **********/
 
-// Serve static files from the `/public` directory:
-// i.e. `/images`, `/scripts`, `/styles`
-app.use(express.static('public'));
+ app.get('/api/headGear', function readShoes(req, res) {
+     res.json({ data: headGear});
+ });
+
 
 /*
  * HTML Endpoints
@@ -48,12 +54,12 @@ app.get('/api', function apiIndex(req, res) {
   res.json({
     woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentationUrl: "https://github.com/namelessprofit/express_self_api/README.md", // CHANGE ME
+    baseUrl: "http://myhats.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "POST", path: "/api/hats", description: "Crate a new hat"} // CHANGE ME
     ]
   })
 });
